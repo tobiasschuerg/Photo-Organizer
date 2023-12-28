214968c2194a0037a0debe7fd80a77559c2d4dd9
Tobias Schürg <tobiasschuerg@gmail.com>
2023-12-28 21:45:22 +0100

    optimizations

--- date_extraction.py
+++ date_extraction.py
@@ -1,19 +1,108 @@
+# date_extractions.py
+import os
 import re
 
 from PIL import Image
+from hachoir.metadata import extractMetadata
+from hachoir.parser import createParser
 
 
-def get_date_taken(path):
+def get_date_from_image_exif(file_path):
     """
-    Returns the date the image at the specified path was taken. If the date cannot be extracted from the EXIF data,
-    attempts to extract the date from the filename. If the date cannot be extracted from the filename either, returns None.
+    Extract the date from the EXIF data of an image.
+
+    Args:
+        file_path: The path of the image file.
+
+    Returns:
+        The date the image was taken in the format YYYY-MM-DD if it is found in the EXIF data, otherwise None.
     """
     try:
-        return Image.open(path)._getexif()[36867]
-    except (KeyError, AttributeError, TypeError, IOError):
-        # Try to extract date from filename
-        date_taken = re.search(r'\d{4}-\d{2}-\d{2}', path)
-        if date_taken:
-            return date_taken.group(0).replace('-', ':')
+        with Image.open(file_path) as img:
+            if hasattr(img, '_getexif'):
+                exif_data = img._getexif()
+                if exif_data is not None and 36867 in exif_data:
+                    return exif_data[36867].replace(':', '-')
+    except Exception as e:
+        print(f"An error occurred while trying to get the date from the EXIF data for file {file_path}: {e}")
+    return None
+
+
+def get_date_from_movie_metadata(file_path):
+    """
+    Extract the date from the metadata of a movie file.
+
+    Args:
+        file_path: The path of the movie file.
+
+    Returns:
+        The date the movie was created in the format YYYY-MM-DD if it is found in the metadata, otherwise None.
+    """
+    try:
+        parser = createParser(file_path)
+        if parser:
+            with parser:
+                metadata = extractMetadata(parser)
+                if metadata:
+                    date_taken = metadata.get('creation_date')
+                    if date_taken:
+                        return str(date_taken.date())
+    except Exception as e:
+        print(f"An error occurred while trying to get the date from the metadata for file {file_path}: {e}")
+    return None
+
+
+def get_date_from_filename(file_path):
+    """
+    Extract the date from the filename.
+
+    Args:
+        file_path: The path of the file.
+
+    Returns:
+        The date in the format YYYY-MM-DD if it is found in the filename, otherwise None.
+    """
+    try:
+        # Try to extract the date from the filename (format: PXL_YYYYMMDD_HHMMSS...)
+        match = re.search(r'PXL_(\d{8})', os.path.basename(file_path))
+        if match:
+            date = match.group(1)
+            return date[:4] + "-" + date[4:6] + "-" + date[6:8]
         else:
-            return None
+            # Try to extract the date from the filename (format: YYYY-MM-DD)
+            match = re.search(r'\b(\d{4}-\d{2}-\d{2})\b', os.path.basename(file_path))
+            if match:
+                return match.group(1)
+    except Exception as e:
+        print(f"An error occurred while trying to get the date from the filename for file {file_path}: {e}")
+    return None
+
+
+def get_date_taken(file_path):
+    """
+    Get the date the file was taken. If the metadata is not available, try to extract the date from the filename.
+
+    Args:
+        file_path: The path of the file.
+
+    Returns:
+        The date the file was taken in the format YYYY-MM-DD if it can be found, otherwise None.
+    """
+    filename, file_extension = os.path.splitext(file_path)
+
+    if file_extension.lower() in ['.jpg', '.jpeg', '.png', '.gif', '.tiff']:
+        date_taken = get_date_from_image_exif(file_path)
+    elif file_extension.lower() in ['.mp', '.mp4', '.mov', '.avi', '.mkv']:
+        date_taken = get_date_from_movie_metadata(file_path)
+    else:
+        print(f"Unsupported file extension {file_extension} for file {file_path}")
+        return None
+
+    if date_taken is None:
+        date_taken = get_date_from_filename(file_path)
+
+    if date_taken is None:
+        print(f"Could not extract date from file {file_path}")
+        return None
+
+    return date_taken
--- file_operations.py
+++ file_operations.py
@@ -1,3 +1,4 @@
+# file_operation.py
 import os
 import shutil
 
@@ -18,6 +19,30 @@ def move_file(source_path, destination_path):
     shutil.move(source_path, destination_path)
 
 
+def get_destination_directory(output_directory, date_taken):
+    """
+    Get the destination directory based on the date taken.
+    """
+    year, month, day = date_taken[:4], date_taken[5:7], date_taken[
+                                                        8:10]  # Extract year, month, and day from the EXIF data or filename
+
+    # Check if a directory starting with the correct date string already exists
+    year_directory = os.path.join(output_directory, year)
+    date_string = f"{year}-{month}-{day}"
+    destination_directory = None
+    if os.path.exists(year_directory):
+        for dir_name in os.listdir(year_directory):
+            if dir_name.startswith(date_string):
+                destination_directory = os.path.join(year_directory, dir_name)
+                break
+
+    if not destination_directory:
+        destination_directory = os.path.join(year_directory, date_string)
+        os.makedirs(destination_directory, exist_ok=True)  # Create new directory if it doesn't exist
+
+    return destination_directory
+
+
 def organize_files(input_directory, output_directory, operation):
     """
     Organizes files in the input directory into subdirectories in the output directory based on the date the file was taken.
@@ -36,21 +61,34 @@ def organize_files(input_directory, output_directory, operation):
         print(f"Output directory {output_directory} already exists and is not a directory.")
         return
 
+    file_count = 0
+    processed_files_count = 0
+    error_files_count = 0
+
     for filename in os.listdir(input_directory):
         if filename.lower().endswith(
-                (".png", ".jpg", ".jpeg", ".gif", ".bmp", ".mp4", ".mov", ".avi", ".mkv", ".flv", ".wmv")):
+                (".png", ".jpg", ".jpeg", ".gif", ".bmp", ".mp", ".mp4", ".mov", ".avi", ".mkv", ".flv", ".wmv")):
+            file_count += 1
             file_path = os.path.join(input_directory, filename)
             date_taken = get_date_taken(file_path)
 
             if date_taken:
-                year, month = date_taken[:4], date_taken[5:7]  # Extract year and month from the EXIF data or filename
+                destination_directory = get_destination_directory(output_directory, date_taken)
+                new_file_path = os.path.join(destination_directory, filename)
 
-                new_directory = os.path.join(output_directory, year, month)
-                os.makedirs(new_directory, exist_ok=True)  # Create new directory if it doesn't exist
+                try:
+                    if operation.lower() == 'move':
+                        move_file(file_path, new_file_path)
+                    else:
+                        copy_file(file_path, new_file_path)
+                    processed_files_count += 1
+                except Exception as e:
+                    print(f"An error occurred while processing file {file_path}: {e}")
+                    error_files_count += 1
+            else:
+                print(f"Could not determine date taken for file {file_path}. Skipping.")
+                error_files_count += 1
 
-                new_file_path = os.path.join(new_directory, filename)
+            print(f"Processed {processed_files_count} out of {file_count} files. {error_files_count} errors occurred.")
 
-                if operation.lower() == 'move':
-                    move_file(file_path, new_file_path)
-                else:
-                    copy_file(file_path, new_file_path)
+    print(f"Finished processing. {processed_files_count} files processed, {error_files_count} errors.")
